37e7f60bc8587083461e27763b21e6c05f023c97
mst923 <kyotoshiya1048@gmail.com>
2025-05-10 13:46:26 +0900

    chinko

--- app/App.js
+++ app/App.js
@@ -4,3 +4,12 @@ import MapWithAvatar from '../components/MapWithAvatar';
 export default function App() {
   return <MapWithAvatar />;
 }
+
+
+// ちんぽこ
+// ちんぽこ
+// ちんぽこ
+// ちんぽこ
+// ちんぽこ
+// ちんぽこ
+// ちんぽこ                         
